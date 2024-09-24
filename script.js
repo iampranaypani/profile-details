98be7e0962c2dc65e237c2079b1ba8a31edd17f8
iampranaypani <153253406+iampranaypani@users.noreply.github.com>
2024-09-24 20:30:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const profileCard = document.querySelector('.profile-card');
+    profileCard.style.animation = 'dropdown 0.6s forwards';
+});
